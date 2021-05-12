a9ee9658f795ef3015c5fca6645807486fbed60b
David Picard <picard@ensea.fr>
2021-05-12 09:50:19 +0200

    init bn with 1.

--- cifar_seed.py
+++ cifar_seed.py
@@ -22,8 +22,8 @@ net.to(device).half()
 for layer in net.modules():
   if isinstance(layer, nn.BatchNorm2d):
     layer.float()
-    # if hasattr(layer, 'weight') and layer.weight is not None:
-    #   layer.weight.data.fill_(1.0)
+    if hasattr(layer, 'weight') and layer.weight is not None:
+      layer.weight.data.fill_(1.0)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.2, momentum=0.9, nesterov=True, weight_decay=0.001)
