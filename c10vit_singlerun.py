0c3f3ebe8ef8633b428481fc41ae66e9137b85f9
David Picard <picard@ensea.fr>
2021-05-13 17:34:48 +0200

    cosine lr

--- c10vit_singlerun.py
+++ c10vit_singlerun.py
@@ -41,7 +41,7 @@ def lr(e):
   else:
     return 0.02
 # sched = optim.lr_scheduler.LambdaLR(optimizer, lr)
-torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epoch, eta_min=0.01)
+sched = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epoch, eta_min=0.01)
 
 augment = Augment()
 augment.to(device).half()
