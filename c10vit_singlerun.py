33f0fba55b97b03c9ba27154bcfd125bd974d10c
David Picard <picard@ensea.fr>
2021-05-13 12:30:28 +0200

    print 2 epo

--- c10vit_singlerun.py
+++ c10vit_singlerun.py
@@ -23,7 +23,7 @@ net.to(device).half()
 
 
 criterion = nn.CrossEntropyLoss()
-criterion2 = CrossEntropyLabelSmooth(num_classes=10, epsilon=0.1)
+criterion2 = CrossEntropyLabelSmooth(num_classes=10, epsilon=0.2)
 optimizer = optim.SGD(net.parameters(), lr=0.5, momentum=0.9, nesterov=True, weight_decay=0.0)
 
 def lr(e):
