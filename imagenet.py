6c2fd23363b4218caa75255b7da71c72af3c6942
David Picard <picard@ensea.fr>
2021-05-18 09:46:49 +0200

    fix stats

--- imagenet.py
+++ imagenet.py
@@ -22,8 +22,8 @@ parser.add_argument("--data_dir", help="path to imagenet")
 args = parser.parse_args()
 
 train, val = build_imagenet(args.data_dir)
-train_ds = DataLoader(train, batch_size=batch_size, num_workers=10, shuffle=True)
-val_ds = DataLoader(val, batch_size=50, num_workers=10)
+train_ds = DataLoader(train, batch_size=batch_size, num_workers=4, shuffle=True)
+val_ds = DataLoader(val, batch_size=v_batch_size, num_workers=4)
 
 # build model
 model = torchvision.models.resnet50(pretrained=True)
@@ -42,7 +42,7 @@ for imgs, lbls in val_ds:
     lbls = lbls.to(device)
     outputs = model(imgs)
     val_loss.append(criterion(outputs, lbls))
-    val_acc.append((outputs.argmax(dim=1) == lbls).sum() / lbls.shape[0])
+    val_acc.append(((outputs.argmax(dim=1) == lbls).sum() / lbls.shape[0]).detach().cpu())
     print('val loss {:5.02f} val acc {:5.02f}'.format(torch.stack(val_loss).mean(), 100. * torch.stack(val_acc).mean()), end='\r')
 print()
 
