e06bda212f997bf8459d5dd537f92f9d3642a1ff
David Picard <picard@ensea.fr>
2021-05-18 09:39:07 +0200

    add initial imagenet

--- datasets.py
+++ datasets.py
@@ -5,6 +5,9 @@ from torchvision import transforms
 import numpy as np
 
 # put all dataset on gpu :)
+from torchvision.datasets import ImageNet
+
+
 def build_dataset(device="cuda"):
     _CIFAR_MEAN, _CIFAR_STD = (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
 
@@ -94,3 +97,25 @@ class Augment(nn.Module):
     def forward(self, x):
         x = self.transforms(x)
         return x
+
+def build_imagenet(data_dir, device="cuda"):
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    transform_val = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    train = ImageNet(data_dir, transform=transform_train)
+    val = ImageNet(data_dir, split='val', transform=transform_val)
+    return train, val
