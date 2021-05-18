d79e386499206b534ad5669b906d9fa2b0161c74
David Picard <picard@ensea.fr>
2021-05-18 09:49:23 +0200

    better val print and setup

--- imagenet.py
+++ imagenet.py
@@ -11,7 +11,7 @@ from datasets import *
 from loss import *
 
 batch_size = 256
-v_batch_size = 50
+v_batch_size = 100
 epoch = 42
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -22,6 +22,8 @@ parser.add_argument("--data_dir", help="path to imagenet")
 args = parser.parse_args()
 
 train, val = build_imagenet(args.data_dir)
+n_train = len(train)
+n_val = len(val)
 train_ds = DataLoader(train, batch_size=batch_size, num_workers=4, shuffle=True)
 val_ds = DataLoader(val, batch_size=v_batch_size, num_workers=4)
 
@@ -37,13 +39,15 @@ criterion = nn.CrossEntropyLoss()
 model.eval()
 val_loss = []
 val_acc = []
+i = 0
 for imgs, lbls in val_ds:
     imgs = imgs.to(device)
     lbls = lbls.to(device)
     outputs = model(imgs)
     val_loss.append(criterion(outputs, lbls).detach().cpu())
     val_acc.append(((outputs.argmax(dim=1) == lbls).sum() / lbls.shape[0]).detach().cpu())
-    print('val loss {:5.02f} val acc {:5.02f}'.format(torch.stack(val_loss).mean(), 100. * torch.stack(val_acc).mean()), end='\r')
+    print('{}/{} val loss {:5.02f} val acc {:5.02f}'.format(i, n_val, torch.stack(val_loss).mean(), 100. * torch.stack(val_acc).mean()), end='\r')
+    i += 1
 print()
 
 
