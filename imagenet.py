68db079114cb8a90d584d3ab7c140a6a43ac28ff
David Picard <picard@ensea.fr>
2021-05-19 11:55:07 +0200

    test multiple seeds

--- imagenet.py
+++ imagenet.py
@@ -47,6 +47,7 @@ parser.add_argument("--data_dir", help="path to imagenet")
 parser.add_argument("--eval_pretrained", type=bool, default=False)
 parser.add_argument("--output", default="results_imagenet.json")
 parser.add_argument("--seed", type=int, default=0)
+parser.add_argument("--nb_seeds", type=int, default=10)
 args = parser.parse_args()
 
 data = []
@@ -61,139 +62,140 @@ val_ds = DataLoader(val, batch_size=v_batch_size, num_workers=10)
 n_train = len(train_ds)
 n_val = len(val_ds)
 
-s = args.seed
-torch.manual_seed(s)
-np.random.seed(s)
+for s in range(args.seed, args.nb_seeds):
+    print('doing seed {}'.format(s))
+    torch.manual_seed(s)
+    np.random.seed(s)
 
-# build model
-model = torchvision.models.resnet50(pretrained=True)
-if not args.eval_pretrained:
-    torch.nn.init.kaiming_uniform_(model.fc.weight)
-    torch.nn.init.normal_(model.fc.bias)
+    # build model
+    model = torchvision.models.resnet50(pretrained=True)
+    if not args.eval_pretrained:
+        torch.nn.init.kaiming_uniform_(model.fc.weight)
+        torch.nn.init.normal_(model.fc.bias)
 
-for p in model.parameters():
-    p.requires_grad = False
-if not args.eval_pretrained:
-    model.fc.weight.requires_grad = True
-    model.fc.bias.requires_grad = True
-
-model.to(device)
-
-
-# optimization hparams
-criterion = nn.CrossEntropyLoss()
-
-if not args.eval_pretrained:
-
-    criterion2 = CrossEntropyLabelSmooth(num_classes=1000, epsilon=0.1)
-    optimizer = optim.SGD(model.parameters(), lr=0.02, momentum=0.9, nesterov=True, weight_decay=0.0001)
-    sched = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epoch*n_train//1000, eta_min=0.001)
-
-    # training loop
-    print('Training last layer')
-    t_start = time.time()
-    i = 1
-    for e in range(epoch):  # loop over the dataset multiple times
-        running_loss = []
-        running_acc = []
-        start = time.time()
-        for imgs, lbls in train_ds:
-            imgs = imgs.to(device)
-            lbls = lbls.to(device)
-
-            optimizer.zero_grad()
-
-            outputs = model(imgs)
-            loss = criterion(outputs, lbls)
-            loss2 = criterion2(outputs, lbls)
-            loss = loss + loss2
-
-            loss.backward()
-            optimizer.step()
-            # print statistics
-            running_loss.append(loss.detach().cpu())
-            running_acc.append(((outputs.argmax(dim=1) == lbls).sum() / lbls.shape[0]).detach().cpu())
-
-            print('{}/{} loss: {:5.02f} acc: {:5.02f} in {:6.01f}'.format(i, n_train, torch.stack(running_loss).mean(), 100*torch.stack(running_acc).mean(), time.time()-start), end='\r')
-
-            if i%100 == 0:
-                print()
-                l, a = eval(model)
-                tr_loss.append(l)
-                tr_acc.append(a)
-                model.train()
-                sched.step()
-
-            if i >= max_train:
-                break
-
-            i += 1
-
-    print('Fine tuning all layers')
-    # new dataset batch_size
-    train_ds = DataLoader(train, batch_size=batch_size_ft, num_workers=10, shuffle=True)
-    n_train = len(train_ds)
-
-    # train all model
     for p in model.parameters():
-        p.requires_grad = True
+        p.requires_grad = False
+    if not args.eval_pretrained:
+        model.fc.weight.requires_grad = True
+        model.fc.bias.requires_grad = True
 
-    # new optim and sched
-    optimizer = optim.SGD(model.parameters(), lr=0.0005, momentum=0.9, nesterov=True, weight_decay=0.0001)
-    sched = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=n_train//2000, eta_min=0.0001)
+    model.to(device)
 
-    i = 1
-    for e in range(ft_epoch):
-        running_loss = []
-        running_acc = []
-        start = time.time()
-        for imgs, lbls in train_ds:
-            imgs = imgs.to(device)
-            lbls = lbls.to(device)
 
-            optimizer.zero_grad()
+    # optimization hparams
+    criterion = nn.CrossEntropyLoss()
 
-            outputs = model(imgs)
-            loss = criterion(outputs, lbls)
-            loss = loss
+    if not args.eval_pretrained:
 
-            loss.backward()
-            optimizer.step()
-            # print statistics
-            running_loss.append(loss.detach().cpu())
-            running_acc.append(((outputs.argmax(dim=1) == lbls).sum() / lbls.shape[0]).detach().cpu())
+        criterion2 = CrossEntropyLabelSmooth(num_classes=1000, epsilon=0.1)
+        optimizer = optim.SGD(model.parameters(), lr=0.02, momentum=0.9, nesterov=True, weight_decay=0.0001)
+        sched = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epoch*n_train//1000, eta_min=0.001)
 
-            print('{}/{} loss: {:5.02f} acc: {:5.02f} in {:6.01f}'.format(i, n_train, torch.stack(running_loss).mean(),
-                                                                          100 * torch.stack(running_acc).mean(),
-                                                                          time.time() - start), end='\r')
+        # training loop
+        print('Training last layer')
+        t_start = time.time()
+        i = 1
+        for e in range(epoch):  # loop over the dataset multiple times
+            running_loss = []
+            running_acc = []
+            start = time.time()
+            for imgs, lbls in train_ds:
+                imgs = imgs.to(device)
+                lbls = lbls.to(device)
 
-            if i % 200 == 0:
-                print()
-                l, a = eval(model)
-                ft_loss.append(l)
-                ft_acc.append(a)
-                model.train()
-                sched.step()
+                optimizer.zero_grad()
 
-            if i >= max_train_ft:
-                break
-            i += 1
+                outputs = model(imgs)
+                loss = criterion(outputs, lbls)
+                loss2 = criterion2(outputs, lbls)
+                loss = loss + loss2
 
-# eval
-if args.eval_pretrained:
-    eval(model)
-d = { 'seed': s,
-    'tr_loss': tr_loss,
-    'tr_acc': tr_acc,
-    'ft_loss': ft_loss,
-    'ft_acc': ft_acc,
-    'time': time.time() - start
-    }
+                loss.backward()
+                optimizer.step()
+                # print statistics
+                running_loss.append(loss.detach().cpu())
+                running_acc.append(((outputs.argmax(dim=1) == lbls).sum() / lbls.shape[0]).detach().cpu())
 
-data.append(d)
-print(d)
-with open(args.output, 'w') as fp:
-    json.dump(data, fp)
+                print('{}/{} loss: {:5.02f} acc: {:5.02f} in {:6.01f}'.format(i, n_train, torch.stack(running_loss).mean(), 100*torch.stack(running_acc).mean(), time.time()-start), end='\r')
+
+                if i%100 == 0:
+                    print()
+                    l, a = eval(model)
+                    tr_loss.append(l)
+                    tr_acc.append(a)
+                    model.train()
+                    sched.step()
+
+                if i >= max_train:
+                    break
+
+                i += 1
+
+        print('Fine tuning all layers')
+        # new dataset batch_size
+        train_ds = DataLoader(train, batch_size=batch_size_ft, num_workers=10, shuffle=True)
+        n_train = len(train_ds)
+
+        # train all model
+        for p in model.parameters():
+            p.requires_grad = True
+
+        # new optim and sched
+        optimizer = optim.SGD(model.parameters(), lr=0.0005, momentum=0.9, nesterov=True, weight_decay=0.0001)
+        sched = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=n_train//2000, eta_min=0.0001)
+
+        i = 1
+        for e in range(ft_epoch):
+            running_loss = []
+            running_acc = []
+            start = time.time()
+            for imgs, lbls in train_ds:
+                imgs = imgs.to(device)
+                lbls = lbls.to(device)
+
+                optimizer.zero_grad()
+
+                outputs = model(imgs)
+                loss = criterion(outputs, lbls)
+                loss = loss
+
+                loss.backward()
+                optimizer.step()
+                # print statistics
+                running_loss.append(loss.detach().cpu())
+                running_acc.append(((outputs.argmax(dim=1) == lbls).sum() / lbls.shape[0]).detach().cpu())
+
+                print('{}/{} loss: {:5.02f} acc: {:5.02f} in {:6.01f}'.format(i, n_train, torch.stack(running_loss).mean(),
+                                                                              100 * torch.stack(running_acc).mean(),
+                                                                              time.time() - start), end='\r')
+
+                if i % 200 == 0:
+                    print()
+                    l, a = eval(model)
+                    ft_loss.append(l)
+                    ft_acc.append(a)
+                    model.train()
+                    sched.step()
+
+                if i >= max_train_ft:
+                    break
+                i += 1
+
+    # eval
+    if args.eval_pretrained:
+        eval(model)
+    d = { 'seed': s,
+        'tr_loss': tr_loss,
+        'tr_acc': tr_acc,
+        'ft_loss': ft_loss,
+        'ft_acc': ft_acc,
+        'time': time.time() - start
+        }
+
+    data.append(d)
+    print(d)
+    with open(args.output, 'w') as fp:
+        json.dump(data, fp)
 
 
 
