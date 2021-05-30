27a65346171712edc69929a8108a67881ee41324
David Picard <picard@ensea.fr>
2021-05-30 14:58:53 +0200

    vit ftlr 0.0002

--- imagenet_ssl.py
+++ imagenet_ssl.py
@@ -154,7 +154,7 @@ for s in range(args.seed, args.seed + args.nb_seeds):
             p.requires_grad = True
 
         # new optim and sched
-        optimizer = optim.SGD(model.parameters(), lr=0.0005, momentum=0.9, nesterov=True, weight_decay=0.0001)
+        optimizer = optim.SGD(model.parameters(), lr=0.0002, momentum=0.9, nesterov=True, weight_decay=0.0001)
         sched = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_train_ft, eta_min=0.0001)
 
         i = 1
